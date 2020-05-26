cf3aa908437eb3e13badd62f7b5a61f36ae3ad98
NamIT <vn.apnic@gmail.com>
2020-05-26 16:35:17 +0700

    create textture

--- android/src/main/kotlin/com/namit/flutter_wowza/FlutterWOWZCameraView.kt
+++ android/src/main/kotlin/com/namit/flutter_wowza/FlutterWOWZCameraView.kt
@@ -3,13 +3,11 @@ package com.namit.flutter_wowza
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraAccessException
 import android.os.Build
 import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.pedro.rtplibrary.rtsp.RtspCamera1
@@ -19,25 +17,20 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.platform.PlatformView
 
+private const val PERMISSIONS_REQUEST_CODE = 0x1
+
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class FlutterWOWZCameraView internal
 constructor(private val context: Context?, private val registrar: PluginRegistry.Registrar,
             private val methodChannel: MethodChannel, id: Int?, params: Map<String, Any>?) :
         PlatformView, MethodChannel.MethodCallHandler,
         PluginRegistry.RequestPermissionsResultListener,
-        SurfaceHolder.Callback{
+        TextureView.SurfaceTextureListener {
 
-    val TAG = "FlutterWOWZCameraView"
+    val tag = "FlutterWOWZCameraView"
 
-    private var mPermissionsGranted = false
-    private var hasRequestedPermissions = false
-    var videoIsInitialized = false
-    var audioIsInitialized = false
-
-    private var surfaceView: SurfaceView = SurfaceView(context)
+    private var mTextureView: TextureView = TextureView(context)
     private var rtspCamera: RtmpCameraView? = null
-
-    private val PERMISSIONS_REQUEST_CODE = 0x1
 
     private var mRequiredPermissions = arrayOf(
             Manifest.permission.CAMERA,
@@ -51,57 +44,76 @@ constructor(private val context: Context?, private val registrar: PluginRegistry
     }
 
     override fun getView(): View {
-        val params = surfaceView.layoutParams
-        params.width = ViewGroup.LayoutParams.MATCH_PARENT
-        params.height = ViewGroup.LayoutParams.MATCH_PARENT
-        surfaceView.layoutParams = params
-        return surfaceView
+        return mTextureView
     }
 
     override fun dispose() {
     }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        context?.let {context->
+        context?.let { context ->
             when (call.method) {
                 "availableCameras" -> try {
                     result.success(CameraUtils.getAvailableCameras(context))
                 } catch (e: Exception) {
                     handleException(e, result)
                 }
-
                 "initialize" -> {
+                    mTextureView.surfaceTextureListener = this@FlutterWOWZCameraView
+                    rtspCamera?.close()
 
+                }
+                // Camera
+                "switchCamera" -> {
+                }
+                "flashLight" -> {
+                }
+                // Streaming control
+                "startBroadcasting" -> {
+                    io.flutter.Log.i(tag, call.arguments.toString())
+                    rtspCamera?.startStreaming(call.argument("url"), result)
+                }
+                "pauseBroadcasting" -> {
+                }
+                "resumeBroadcasting" -> {
+                }
+                "stopBroadcasting" -> {
+                }
+                // dispose
+                "dispose" -> {
+                    rtspCamera?.dispose()
+                    result.success(null)
+                }
+                else -> {
                 }
             }
         }
 
     }
 
+    override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+        instantiateCamera()
+    }
+
+    override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
+        // Ignored, the Camera does all the work for us
+    }
+
+    override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
+        // Update your view here!
+    }
+
+    override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
+        rtspCamera?.dispose()
+        return true
+    }
+
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     @Throws(CameraAccessException::class)
-    private fun instantiateCamera(call: MethodCall, result: MethodChannel) {
-        val cameraName = call.argument<String>("cameraName")
-        val resolutionPreset = call.argument<String>("resolutionPreset")
-        val enableAudio = call.argument<Boolean>("enableAudio")!!
-        val invokeMessages = InvokeMessages(result)
-        rtspCamera = RtmpCameraView(registrar.activity(),surfaceView,invokeMessages,cameraName,resolutionPreset,enableAudio)
+    private fun instantiateCamera(surface: SurfaceTexture?, width: Int, height: Int) {
+        rtspCamera = RtmpCameraView(registrar.activity(), surface)
     }
 
-    /**
-     * SurfaceHolder.Callback
-     */
-    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
-        Log.i(TAG, "surfaceChanged")
-    }
-
-    override fun surfaceDestroyed(p0: SurfaceHolder?) {
-        Log.i(TAG, "surfaceDestroyed")
-    }
-
-    override fun surfaceCreated(p0: SurfaceHolder?) {
-        Log.i(TAG, "surfaceCreated")
-    }
 
     // We move catching CameraAccessException out of onMethodCall because it causes a crash
     // on plugin registration for sdks incompatible with Camera2 (< 21). We want this plugin to
@@ -114,126 +126,8 @@ constructor(private val context: Context?, private val registrar: PluginRegistry
         throw (exception as RuntimeException)
     }
 
-    private fun requestPermissionToAccess(): Boolean {
-        var result = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            result = if (mRequiredPermissions.isNotEmpty()) checkPermissionToAccess(mRequiredPermissions) else true
-
-            if (!result && !hasRequestedPermissions) {
-                ActivityCompat.requestPermissions(registrar.activity(),
-                        mRequiredPermissions,
-                        PERMISSIONS_REQUEST_CODE)
-                hasRequestedPermissions = true
-            }
-        }
-        return result
-    }
-
-    private fun requestPermissionToAccess(source: String): Boolean {
-        var result = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            result = if (mRequiredPermissions.isNotEmpty()) checkPermissionToAccess(source) else true
-            if (!result && !hasRequestedPermissions) {
-                ActivityCompat.requestPermissions(registrar.activity(),
-                        mRequiredPermissions,
-                        PERMISSIONS_REQUEST_CODE)
-                hasRequestedPermissions = true
-            }
-        }
-        return result
-    }
-
-    private fun checkPermissionToAccess(permissions: Array<String>): Boolean {
-        var result = true
-//        if (goCoderBroadcaster != null) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                for (permission in permissions) {
-//                    if (ContextCompat.checkSelfPermission(registrar.activity(), permission) == PackageManager.PERMISSION_DENIED) {
-//                        result = false
-//                    }
-//                }
-//            }
-//        } else {
-//            Log.e("FlutterWOWZCameraView", "goCoderBroadcaster is null!")
-//            result = false
-//        }
-        return result
-    }
-
-    private fun checkPermissionToAccess(source: String): Boolean {
-//        if (goCoderBroadcaster != null) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                if (ContextCompat.checkSelfPermission(registrar.activity(), source) == PackageManager.PERMISSION_DENIED) {
-//                    return false
-//                }
-//            }
-//        } else {
-//            Log.e("FlutterWOWZCameraView", "goCoderBroadcaster is null!")
-//            return false
-//        }
-        return true
-    }
-
+    // Request permissions
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray?): Boolean {
-        mPermissionsGranted = true
-        when (requestCode) {
-            PERMISSIONS_REQUEST_CODE -> {
-                for (grantResult in grantResults!!) {
-                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                        mPermissionsGranted = false
-                    }
-                }
-                hasRequestedPermissions = false
-                onPermissionResult(mPermissionsGranted)
-            }
-        }
-
-        Log.i("FlutterWOWZCameraView", "onRequestPermissionsResult  has requested: $hasRequestedPermissions")
-        return true
-    }
-
-    private fun onPermissionResult(mPermissionsGranted: Boolean) {
-//        if (goCoder != null) {
-//            if (mPermissionsGranted) {
-//                // Initialize the camera preview
-//                if (requestPermissionToAccess(Manifest.permission.CAMERA)) {
-//                    if(!videoIsInitialized) {
-//                        val availableCameras = goCoderCameraView.cameras
-//                        // Ensure we can access to at least one camera
-//                        if (availableCameras.isNotEmpty()) {
-//                            // Set the video broadcaster in the broadcast config
-//                            goCoderBroadcastConfig?.videoBroadcaster = goCoderCameraView
-//                            videoIsInitialized = true
-//                            Log.i("FlutterWOWZCameraView", "*** getOriginalFrameSizes - Get original frame size : ")
-//                        } else {
-//                            Log.e("FlutterWOWZCameraView", "Could not detect or gain access to any cameras on this device")
-//                            goCoderBroadcastConfig?.isVideoEnabled = false
-//                        }
-//                    }
-//                } else {
-//                    Log.e("FlutterWOWZCameraView", "Exception Fail to connect to camera service. I checked camera permission in Settings")
-//                }
-//
-//                if (requestPermissionToAccess(Manifest.permission.RECORD_AUDIO)) {
-//                    if(!audioIsInitialized) {
-//                        // Create an audio device instance for capturing and broadcasting audio
-//                        goCoderAudioDevice = WOWZAudioDevice()
-//                        // Set the audio broadcaster in the broadcast config
-//                        goCoderBroadcastConfig?.audioBroadcaster = goCoderAudioDevice
-//                        audioIsInitialized = true
-//                    }
-//                } else {
-//                    Log.e("FlutterWOWZCameraView", "Exception Fail to connect to record audio service. I checked camera permission in Settings")
-//                }
-//
-//                if (videoIsInitialized && audioIsInitialized) {
-//                    Log.i("FlutterWOWZCameraView", "startPreview")
-//                    if (!goCoderCameraView.isPreviewing)
-//                        goCoderCameraView.startPreview()
-//                }
-//            }
-//        } else {
-//            Log.e("FlutterWOWZCameraView", "goCoder is null!, Please check the license key GoCoder SDK, maybe your license key GoCoder SDK is wrong!")
-//        }
+        TODO("Not yet implemented")
     }
 }
--- android/src/main/kotlin/com/namit/flutter_wowza/RtmpCameraView.kt
+++ android/src/main/kotlin/com/namit/flutter_wowza/RtmpCameraView.kt
@@ -8,26 +8,33 @@ import android.media.MediaRecorder
 import android.util.Size
 import com.pedro.rtplibrary.util.BitrateAdapter
 import android.content.Context
+import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraManager
 import android.os.Build
 import android.view.OrientationEventListener
+import android.view.Surface
 import android.view.SurfaceView
+import android.view.TextureView
 import androidx.annotation.RequiresApi
 import com.pedro.rtplibrary.rtmp.RtmpCamera2
+import io.flutter.plugin.common.MethodChannel
 import net.ossrs.rtmp.ConnectCheckerRtmp
+import java.io.IOException
+import java.util.*
 import kotlin.math.roundToInt
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class RtmpCameraView internal
 constructor(val activity: Activity,
-            private val surfaceView: SurfaceView,
+            private val mTextureView: TextureView,
+            private val surfaceView: SurfaceTexture,
             private val invokeMessages: InvokeMessages,
             private val cameraName: String?,
             private val resolutionPreset: String?,
-            val enableAudio: Boolean) : ConnectCheckerRtmp   {
+            val enableAudio: Boolean) : ConnectCheckerRtmp {
 
     private val cameraManager: CameraManager = activity.getSystemService(Context.CAMERA_SERVICE) as CameraManager
-    private var rtmpCamera: RtmpCamera2? = RtmpCamera2(surfaceView,this)
+    private var rtmpCamera: RtmpCamera2? = RtmpCamera2(mTextureView, this)
 
     private val orientationEventListener: OrientationEventListener
     private val isFrontFacing: Boolean
@@ -43,7 +50,7 @@ constructor(val activity: Activity,
     private var mediaRecorder: MediaRecorder? = null
     private var recordingVideo = false
     private var recordingRtmp = false
-    private val recordingProfile: CamcorderProfile
+    private val camcorderProfile: CamcorderProfile
     private var currentOrientation = OrientationEventListener.ORIENTATION_UNKNOWN
 
     private var bitrateAdapter: BitrateAdapter? = null
@@ -65,10 +72,10 @@ constructor(val activity: Activity,
         sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
         isFrontFacing = characteristics.get(CameraCharacteristics.LENS_FACING) == CameraMetadata.LENS_FACING_FRONT
         val preset = ResolutionPreset.valueOf(resolutionPreset!!)
-        recordingProfile = CameraUtils.getBestAvailableCamcorderProfileForResolutionPreset(cameraName, preset)
-        captureSize = Size(recordingProfile.videoFrameWidth, recordingProfile.videoFrameHeight)
+        camcorderProfile = CameraUtils.getBestAvailableCamcorderProfileForResolutionPreset(cameraName, preset)
+        captureSize = Size(camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight)
         previewSize = CameraUtils.computeBestPreviewSize(cameraName, preset)
-        streamSize = Size(recordingProfile.videoFrameWidth, recordingProfile.videoFrameHeight)
+        streamSize = Size(camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight)
     }
 
     /**
@@ -117,19 +124,172 @@ constructor(val activity: Activity,
         }
     }
 
-    /**
-     * Event
-     */
+    private val mediaOrientation: Int
+        get() {
+            val sensorOrientationOffset = if (currentOrientation == OrientationEventListener.ORIENTATION_UNKNOWN) 0 else if (isFrontFacing) -currentOrientation else currentOrientation
+            return (sensorOrientationOffset + sensorOrientation + 360) % 360
+        }
+
+    @Throws(CameraAccessException::class)
+    fun startPreview() {
+        createCaptureSession(CameraDevice.TEMPLATE_PREVIEW, pictureImageReader!!.surface)
+    }
+
+    @Throws(CameraAccessException::class)
+    private fun createCaptureSession(templateType: Int, vararg surfaces: Surface) {
+        createCaptureSession(templateType, null, *surfaces)
+    }
+
+    @Throws(CameraAccessException::class)
+    private fun createCaptureSession(
+            templateType: Int, onSuccessCallback: Runnable?, vararg surfaces: Surface) {
+        // Close any existing capture session.
+        closeCaptureSession()
+
+        // Create a new capture builder.
+        captureRequestBuilder = cameraDevice!!.createCaptureRequest(templateType)
+
+        // Build Flutter surface to render to
+        val surfaceTexture = flutterTexture.surfaceTexture()
+        surfaceTexture.setDefaultBufferSize(previewSize.width, previewSize.height)
+        val flutterSurface = Surface(surfaceView)
+        captureRequestBuilder!!.addTarget(flutterSurface)
+        val remainingSurfaces = listOf(*surfaces)
+        if (templateType != CameraDevice.TEMPLATE_PREVIEW) {
+            // If it is not preview mode, add all surfaces as targets.
+            for (surface in remainingSurfaces) {
+                captureRequestBuilder!!.addTarget(surface)
+            }
+        }
+
+        // Prepare the callback
+        val callback: CameraCaptureSession.StateCallback = object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) {
+                try {
+                    if (cameraDevice == null) {
+                        dartMessenger.send(
+                                DartMessenger.EventType.ERROR, "The camera was closed during configuration.")
+                        return
+                    }
+                    cameraCaptureSession = session
+                    captureRequestBuilder!!.set(
+                            CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+                    cameraCaptureSession!!.setRepeatingRequest(captureRequestBuilder!!.build(), null, null)
+                    onSuccessCallback?.run()
+                } catch (e: CameraAccessException) {
+                    dartMessenger.send(DartMessenger.EventType.ERROR, e.message)
+                } catch (e: IllegalStateException) {
+                    dartMessenger.send(DartMessenger.EventType.ERROR, e.message)
+                } catch (e: IllegalArgumentException) {
+                    dartMessenger.send(DartMessenger.EventType.ERROR, e.message)
+                }
+            }
+
+            override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+                dartMessenger.send(
+                        DartMessenger.EventType.ERROR, "Failed to configure camera session.")
+            }
+        }
+
+        // Collect all surfaces we want to render to.
+        val surfaceList: MutableList<Surface> = ArrayList()
+        surfaceList.add(flutterSurface)
+        surfaceList.addAll(remainingSurfaces)
+        // Start the session
+        cameraDevice!!.createCaptureSession(surfaceList, callback, null)
+    }
 
     private fun closeCaptureSession() {
-        cameraCaptureSession?.run {
-            close()
-        }
+        cameraCaptureSession?.close()
         cameraCaptureSession = null
     }
 
+    @Throws(IOException::class)
+    private fun prepareRtmpPublished(url: String) {
+        rtmpCamera?.prepareAudio()
+        rtmpCamera?.prepareVideo(
+                streamSize.width,
+                streamSize.height,
+                2,
+                1200 * 1024,
+                false,
+                mediaOrientation)
+    }
+
+
+    fun startStreaming(url: String?, result: MethodChannel.Result) {
+        if (url == null) {
+            result.error("fileExists", "Must specify a url.", null)
+            return
+        }
+        try {
+            prepareRtmpPublished(url)
+            recordingRtmp = true
+            result.success(null)
+        } catch (e: CameraAccessException) {
+            result.error("videoRecordingFailed", e.message, null)
+        } catch (e: IOException) {
+            result.error("videoRecordingFailed", e.message, null)
+        }
+    }
+
+    fun stopVideoStreaming(result: MethodChannel.Result) {
+        if (!recordingRtmp) {
+            result.success(null)
+            return
+        }
+        try {
+            recordingRtmp = false
+            rtmpCamera?.stopStream()
+            startPreview()
+            result.success(null)
+        } catch (e: CameraAccessException) {
+            result.error("videoStreamingFailed", e.message, null)
+        } catch (e: IllegalStateException) {
+            result.error("videoStreamingFailed", e.message, null)
+        }
+    }
+
+    fun pauseVideoStreaming(result: MethodChannel.Result) {
+        if (!recordingRtmp) {
+            result.success(null)
+            return
+        }
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                rtmpCamera?.stopStream()
+            } else {
+                result.error("videoStreamingFailed", "pauseVideoStreaming requires Android API +24.", null)
+                return
+            }
+        } catch (e: IllegalStateException) {
+            result.error("videoStreamingFailed", e.message, null)
+            return
+        }
+        result.success(null)
+    }
+
+    fun resumeVideoStreaming(url: String?, result: MethodChannel.Result) {
+        if (!recordingRtmp) {
+            result.success(null)
+            return
+        }
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                rtmpCamera?.startStream()
+            } else {
+                result.error(
+                        "videoStreamingFailed", "resumeVideoStreaming requires Android API +24.", null)
+                return
+            }
+        } catch (e: IllegalStateException) {
+            result.error("videoStreamingFailed", e.message, null)
+            return
+        }
+        result.success(null)
+    }
+
     fun close() {
-        closeCaptureSession()
         cameraDevice?.run {
             close()
         }
